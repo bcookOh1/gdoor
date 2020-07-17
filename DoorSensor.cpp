a7b25dc93307524616deaba281fe9a42b3b0cdcb
BennettCook <bcook845@live.com>
2020-07-17 09:12:41 -0400

    new class for door algorithm

---
+++ DoorSensor.cpp
@@ -0,0 +1,9 @@
+
+
+#include "DoorSensor.h"
+
+DoorSensor::DoorSensor(){
+} // end ctor 
+
+DoorSensor::~DoorSensor(){
+} // end dtor 
---
+++ DoorSensor.h
@@ -0,0 +1,31 @@
+/// file: DoorSensor.h header for DoorSensor class  
+/// author: Bennett Cook
+/// date: 07-17-2020
+/// description: 
+
+
+// header guard
+#ifndef DOORSENSOR_H
+#define DOORSENSOR_H
+
+#include <string>
+#include <map>
+#include <vector>
+
+#include "CommonDef.h"
+#include "Util.h"
+
+
+class DoorSensor {
+
+public:
+   DoorSensor();
+   ~DoorSensor();
+
+private:
+
+
+
+} // end class
+
+#endif // end header guard
