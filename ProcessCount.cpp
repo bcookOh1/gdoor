af27cca1664eb67a4fb4ee72ff998f49350e831e
BennettCook <bcook845@live.com>
2020-08-20 11:48:41 -0400

    update code comments

--- ProcessCount.cpp
+++ ProcessCount.cpp
@@ -1,4 +1,9 @@
 
+// file: ProcessCount.h header for ProcessCount
+// author: Bennett Cook
+// date: 08-20-2020
+// 
+
 #include "ProcessCount.h"
 
 
@@ -9,31 +14,43 @@ ProcessCount::ProcessCount(const string &name){
 ProcessCount::~ProcessCount(){
 } // dtor 
 
+// return the number of running processes call _name
+// use the linux pidof command to get the process count. pidof  prints a space delimited 
+// list of process ids for a process name
 // ref: https://www.boost.org/doc/libs/1_74_0/doc/html/process.html#boost_process.introduction
 int ProcessCount::GetCount(){
 
-   ostringstream oss;
-   ipstream pipe_stream;
-   string cmd = "pidof ";
-   cmd += _name;
+   ostringstream oss;       // used to accumulate the stdout response
+   ipstream pipe_stream;    // used by the boost process child call 
+                            // to capture the stdout response 
+   string cmd = "pidof ";   // the linux command to get the process ids  
+   cmd += _name;            // add the process name to the command
 
+   // start a child process and 
    child c(cmd.c_str(), std_out > pipe_stream);
+
+   // give the child some time to run
    this_thread::sleep_for(chrono::milliseconds(1000));
 
+   // accumulate each stdout line to oss 
    string line;
-
    while (pipe_stream && getline(pipe_stream, line) && !line.empty()) {
       oss << line << " ";
    } // end while 
 
+   // wait for thr child to finish 
    c.wait();
 
+   // copy to output string and tgrim left/right spaces, the nunber of splitVec 
+   // is the process count
    string output = oss.str();
    trim(output);
 
+   // use boost split to make vector of process ids, 
    vector<string> splitVec;
    split(splitVec, output, is_any_of(" ")); 
 
+   // return the number of process ids
    return static_cast<int>(splitVec.size());
 } // end GetCount
 
--- ProcessCount.h
+++ ProcessCount.h
@@ -1,9 +1,12 @@
-
-
-// file TestSingleton.h
+// file ProcessCount.h
 // author: BCook
-// date: 08/12/2020 
-// description: used in linux, test if an instance of this program already running
+// date: 08/20/2020 
+// description: used in linux/unix only, test if an instance of this program already running.
+// Because the gdoor uses gpio for input/output there can on be one instance running, the
+// GetCount() member returns the number of running processes for the constructor param
+// "name". The caller then decides the number of processes to allow. 
+// remarks: This doesn't actually need to be a class since GetCount() does all of the work, but 
+// ProcessCount evolved from an earlier version and may change again so the class was kept 
 
 // header guard
 #ifndef TESTSINGLETON_H
@@ -25,13 +28,16 @@ using namespace boost::process;
 class ProcessCount {
 public:
 
+   // constructor, sets the name fo the process to check
    ProcessCount(const string &name);
    ~ProcessCount();
 
+   // returns the number running process called _name.  
    int GetCount();
 
 private:
 
+   // the process name for the numbr of instances to count  
    string _name;
 
 }; // end ProcessCount 
